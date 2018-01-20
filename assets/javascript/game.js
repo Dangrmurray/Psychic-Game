15250b0bb5b2987d864bd96d392bb84615ca599e
Daniel Murray <danielmurray@Daniels-MacBook-Pro.local>
2018-01-19 22:10:04 -0800

    <comment>

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,46 +1,8 @@
-// var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-//     // Creating variables to hold the number of wins, losses. They start at 0.
-//     var wins = 0;
-//     var losses = 0;
-//     var guesses = 9;
-//     // var guessSoFar = 
-
-//      // var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-//       console.log(computerGuess)
-
-
-//     // This function is run whenever the user presses a key.
-//     document.onkeyup = function(event) {
-
-//       // Determines which key was pressed.
-//       var userGuess = event.key;
-
-
-     
-
-//       if(userGuess === computerGuess){
-//         wins++;
-//     }else{
-//         guesses--;
-//     }
-
-//     if(guesses === 0){
-//         losses++
-//         guesses = 9;
-//     }
-      
-//     document.getElementById('wins').innerHTML = "Wins: " + wins;
-//     document.getElementById('losses').innerHTML = "losses: " + losses;
-//     document.getElementById('guesses').innerHTML = "Guesses left: " + guesses;
-//     document.getElementById('guessSoFar').innerHTML = "Your Guesses so far: " + userGuess;
-
-        
-//       }
       
      var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-    // Creating variables to hold the number of wins, losses, and ties. They start at 0.
+    // Creating variables to hold the number of wins, losses. They start at 0.
     var wins = 0;
     var losses = 0;
 
@@ -55,7 +17,7 @@
       guessSoFar.push(userGuess);
 
 
-      // Randomly chooses a choice from the options array. This is the Computer's guess.
+     
 
 
     if(userGuess === computerGuess) {
